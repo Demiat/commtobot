82096a155e81bfd5066e88387be9c5a4a39a357a
tdv <demiat@mail.ru>
2025-02-10 17:09:58 +0300

    next bot

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ bot = TeleBot(token=DEMIAT_BOT_TOKEN)
 bot_memory_keyboard = types.InlineKeyboardMarkup()
 button_bot_mem = types.InlineKeyboardButton(
     text='Память Робота',
-    callback_data='aaa'
+    callback_data='memory'
 )
 bot_memory_keyboard.add(button_bot_mem)
 
@@ -63,6 +63,19 @@ if not os.path.exists('data_users.pkl'):  # if not exist file, create!
     fdu = open('data_users.pkl', 'wb')
     pickle.dump(dict(), fdu)
     fdu.close()
+
+def open_database():
+    f = open('data_users.pkl', 'r+b')
+    prtl.lock(f, prtl.LOCK_EX)
+    data_users = pickle.load(f)
+    return data_users, f
+
+def close_database(f, data_users=None):
+    f.seek(0)
+    pickle.dump(data_users, f)
+    f.flush()
+    prtl.unlock(f)
+    f.close()
 
 
 def check_envs():
@@ -98,31 +111,52 @@ def send_message(pars_to_send, stype=None):
         )
 
 
+@bot.callback_query_handler()
+def handle_callback(call):
+    user_id = call.from_user.id
+    data_users = open_database()
+    if call.data == 'memory':
+        store_hys = data_users[user_id]['history']
+        if store_hys == 'yes':
+            data_users[user_id]['history'] = 'no'
+            txt = 'Робот отключил свою память!'
+        else:
+            data_users[user_id]['history'] = 'yes'
+            txt = 'Память робота включена!'
+        repl = ms.CAP.format(config.QUERY_LIM_AT_DAY,
+                          data_users[user_id]['history'])
+        try:
+            bot.answer_callback_query(call.id, text=txt)
+            bot.edit_message_caption(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                caption=repl,
+                reply_markup=bot_memory_keyboard,
+                parse_mode='HTML'
+            )
+        except: pass
+
+
 @bot.message_handler(commands=['start'])
 def wake_up(message):
     """Старт!"""
     user_id = message.chat.id
 
-    with open('data_users.pkl', 'r+b') as f:
-        prtl.lock(f, prtl.LOCK_EX)
-        data_users = pickle.load(f)
-        if user_id not in data_users:  # if user new
-            data_users[user_id] = {
-                'name': message.chat.username,
-                'count_query': 0,
-                'query_limit': 0,
-                'count_tokens': 0,
-                'last_enter': dt.datetime.now().strftime("%Y-%m-%d"),
-                'history': 'ВЫКЛ'
-            }
-            f.seek(0)
-            pickle.dump(data_users, f)
-            f.flush()
-            prtl.unlock(f)
+    data_users, f = open_database()
+    if user_id not in data_users:  # if user new
+        data_users[user_id] = {
+            'name': message.chat.username,
+            'count_query': 0,
+            'query_limit': 0,
+            'count_tokens': 0,
+            'last_enter': dt.datetime.now().strftime("%Y-%m-%d"),
+            'history': 'ВЫКЛ'
+        }
+    close_database(data_users, f)
 
     pars_to_send = dict(
         chat_id=message.chat.id,
-        photo=open('static/dembot.jpg', 'rb'),
+        photo=open(config.DEMBOT_PHOTO, 'rb'),
         caption=ms.CAP.format(
             name=message.chat.first_name,
             lim_at_day=config.QUERY_LIM_AT_DAY,
@@ -139,6 +173,7 @@ def main():
     # os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(
     #     pwd, 'static', 'russian_trusted_root_ca.cer'
     # )
+    check_envs()
     logger.info(ms.START_LOG_MESSAGE)
     bot.polling()
 
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 QUERY_LIM_AT_DAY = 100
+DEMBOT_PHOTO = 'static/dembot.jpg'
